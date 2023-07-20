b7bd8311dda84946d2c50b76a1fe868a3750f3f7
saket0707 <saketsm7@gmail.com>
2023-07-20 22:14:50 +0530

    Version_1

---
+++ src/Body.js
@@ -0,0 +1,88 @@
+import React from 'react'
+import "./Body.css"
+import Header from './Header'
+import { useDataLayerValue } from './DataLayer'
+import { Favorite, MoreHoriz, PlayCircleFilled } from '@mui/icons-material';
+import SongRow from './SongRow';
+
+
+function Body({spotify}) {
+
+  const [{discover_weekly}, dispatch] = useDataLayerValue();
+
+  const playPlaylist = (id) => {
+    spotify
+      .play({
+        context_uri: `spotify:playlist:37i9dQZEVXcJqRXIgZ2yOH`,
+      })
+      .then((res) => {
+        spotify.getMyCurrentPlayingTrack().then((r) => {
+          dispatch({
+            type: "SET_ITEM",
+            item: r.item,
+          });
+          dispatch({
+            type: "SET_PLAYING",
+            playing: true,
+          });
+        });
+      });
+  };
+
+  const playSong = (id) => {
+    spotify
+      .play({
+        uris: [`spotify:track:${id}`],
+      })
+      .then((res) => {
+        spotify.getMyCurrentPlayingTrack().then((r) => {
+          dispatch({
+            type: "SET_ITEM",
+            item: r.item,
+          });
+          dispatch({
+            type: "SET_PLAYING",
+            playing: true,
+          });
+        });
+      });
+  };
+
+
+
+
+  return (
+    <div className='body'>
+      <Header spotify={spotify} />
+      <div className="body__info">
+        <img 
+        src={discover_weekly?.images[0].url} 
+        alt='' 
+        />
+        <div className="body__infoText">
+          <strong>PLAYLIST</strong>
+          <h2>Discover Weekly</h2>
+          <p>{discover_weekly?.description}</p>
+        </div>
+      </div>
+
+      <div className="body__songs">
+        <div className="body__icons">
+          <PlayCircleFilled 
+          className='body__shuffle' 
+          onClick={playPlaylist}
+          />
+          <Favorite fontSize='large'/>
+          <MoreHoriz/>
+        </div>
+      </div>
+
+      {discover_weekly?.tracks.items.map((item)=>(
+        <SongRow  playSong={playSong} track={item.track} />
+      ))}
+
+    </div>
+  )
+}
+
+export default Body
---
+++ src/spotify.js
@@ -0,0 +1,30 @@
+//https://developer.spotify.com/
+//documentation/web-playback-sdk/quick-start/#
+
+
+export const authEndpoint = "https://accounts.spotify.com/authorize"
+const redirectUri = "http://localhost:3000/";
+const clientId = "2ccda151b149438cbdce02d9669948e1";
+
+const scopes = [
+    "user-read-currently-playing",
+    "user-read-recently-played",
+    "user-read-playback-state",
+    "user-top-read",
+    "user-modify-playback-state",
+];
+
+export const getTokenFromUrl = () =>{
+    return window.location.hash
+        .substring(1)
+        .split("&")
+        .reduce((initial,item)=>{
+            var parts = item.split("=");
+            initial[parts[0]]=
+            decodeURIComponent(parts[1]);
+
+            return initial;
+        },{});
+}
+
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialogue=true`;
